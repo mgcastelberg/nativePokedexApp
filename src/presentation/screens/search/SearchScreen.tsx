bf4dcc67c59d953073cedc5238662bdce650f0eb
Manuel Gomez <jmgc@virket.com>
2024-06-04 09:08:50 -0600

    Estructura Archivos e Instalacion Stack Navigator

---
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -0,0 +1,12 @@
+
+import { Text, View } from 'react-native'
+
+export const SearchScreen = () => {
+    return (
+        <View>
+            <Text>
+                SearchScreen
+            </Text>
+        </View>
+    )
+}
