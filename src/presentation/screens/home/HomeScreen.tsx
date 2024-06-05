d18d2e643839f17ef6566251ff34ad7ad9569ce8
Manuel Gomez <jmgc@virket.com>
2024-06-05 08:53:19 -0600

    Estrutura de directorios, React Native Paper

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,5 +1,6 @@
 
 import { Text, View } from 'react-native'
+import { Button } from 'react-native-paper'
 
 export const HomeScreen = () => {
     return (
@@ -7,6 +8,9 @@ export const HomeScreen = () => {
             <Text>
                 HomeScreen
             </Text>
+            <Button mode="contained" onPress={() => console.log('Pressed')}>
+                Press me
+            </Button>
         </View>
     )
 }
