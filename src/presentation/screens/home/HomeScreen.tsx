5f4150f2dfa00bd314789c88ae5d4a971b1bce5b
Manuel Gomez <jmgc@virket.com>
2024-06-05 09:28:04 -0600

    Tema light y dark automático

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,11 @@
 
-import { Text, View } from 'react-native'
-import { Button } from 'react-native-paper'
+import { View } from 'react-native'
+import { Button, Text } from 'react-native-paper'
 
 export const HomeScreen = () => {
     return (
         <View>
-            <Text>
+            <Text variant='headlineSmall'>
                 HomeScreen
             </Text>
             <Button mode="contained" onPress={() => console.log('Pressed')}>
