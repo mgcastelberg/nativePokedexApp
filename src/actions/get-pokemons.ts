c63de72769b07f26e88d1dbf536e408d403b6c4d
Manuel Gomez <jmgc@virket.com>
2024-06-07 09:17:36 -0600

    Interfaces de PokeAPI

--- src/actions/get-pokemons.ts
+++ src/actions/get-pokemons.ts
@@ -1,19 +1,34 @@
 import { pokeApi } from "../config/api/pokeApi";
-import { Pokemon } from "../domain/entities/pokemon";
+import type { Pokemon } from "../domain/entities/pokemon";
+import type { PokeAPIPaginatedResponse, PokeAPIPokemon } from "../infraestructure/interfaces/pokeapi.interfaces";
 
 // Solo para simular retraso en una respuesta
 export const sleep = async() => {
     return new Promise( resolve => setTimeout(resolve,2000));
 }
 
-export const getPokemons = async ():Promise<Pokemon[]> => {
+export const getPokemons = async ( page: number, limit: number=20 ):Promise<Pokemon[]> => {
 
     await sleep(); //solo para pruebas intentando mostrar un retraso
 
     try {
-        const url = '/pokemon';
-        const { data } = await pokeApi.get(url);
+        const url = `/pokemon?offset=${ page * 10 }&limit=${ limit }`;
+        const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+        
+        // data.results[0].name
 
+        // No usar async y await por que necesitamos las 20 peticiones en paralelo
+        const pokemonPromises = data.results.map( (info) => {
+            return pokeApi.get<PokeAPIPokemon>(info.url);
+        });
+
+        // Esperar a que todas las promesas se terminen
+        const pokeApiPokemons = await Promise.all(pokemonPromises);
+
+        // Como trae mucha data crearemos un mapper para que filtremos solo la data necesaria
+        // toDo
+
+        console.log(JSON.stringify(pokeApiPokemons,null,2));
         console.log(JSON.stringify(data,null,2));
 
         return [];
